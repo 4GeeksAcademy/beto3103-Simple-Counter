04834e872c6dc3262cb89468a76a57befdbe1e7e
Roberto González Monge <48779442+beto3103@users.noreply.github.com>
2023-09-20 04:40:20 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,34 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faClock, faSolid } from "@fortawesome/free-solid-svg-icons"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import propTypes from "prop-types";
 
 //create your first component
-const Home = () => {
+const Home = (props) => {
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+		<div className="text-center justify-content-center d-flex bg-dark text-light">
+			<FontAwesomeIcon icon = {faClock} size="2xl" className="text-center justify-content-center mt-2 me-2"/>
+			<div className="ms-2">
+				<h1>{props.five}</h1>
+			</div>
+			<div className="ms-2">
+				<h1>{props.four}</h1>
+			</div>
+			<div className="ms-2">
+				<h1>{props.three}</h1>
+			</div>
+			<div className="ms-2">
+				<h1>{props.two}</h1>
+			</div>
+			<div className="ms-2">
+				<h1>{props.one}</h1>
+			</div>
+			
+		</div>
 		</div>
 	);
 };
--- src/js/index.js
+++ src/js/index.js
@@ -8,5 +8,19 @@ import "../styles/index.css";
 //import your own components
 import Home from "./component/home.jsx";
 
+let counter = 0
+
+setInterval(() => {
+    let one = Math.floor((counter / 1) % 10)
+    let two = Math.floor((counter / 10) % 10)
+    let three = Math.floor((counter / 100) % 10)
+    let four = Math.floor((counter / 1000) % 10)
+    let five = Math.floor((counter / 10000) % 10)
+    let six = Math.floor((counter / 100000) % 10)
+    counter++
+    ReactDOM.render
+    ReactDOM.render(<Home one={one} two={two} three={three} four={four} five={five} six={six}/>, document.querySelector("#app"));
+}, 1000)
+
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+
